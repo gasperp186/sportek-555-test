794df7f734cde941c6e6899f484b80f8c32635db
gasperp186 <gasperp186@gmail.com>
2026-04-19 19:55:44 +0100

    bracket4

--- src/components/Brackets/viewer/Bracket4.js
+++ src/components/Brackets/viewer/Bracket4.js
@@ -33,103 +33,46 @@ export default function Bracket4({ matches, basePath = "", isExport = false }) {
 
   return (
     <div className={isExport ? classes.dvaDela2 : classes.dvaDela}>
-      <div className={isExport ? classes.container2 : classes.container}>
-        
-        {/* 1. STOLPEC: POLFINALE 1 (Na mobitelu order: 1) */}
-        <div className={`
-          ${isExport ? classes.col2 : classes.col} 
-          ${isExport ? classes.colSemi2 : classes.colSemi} 
-          ${classes.order1}
-        `}>
-          <Match
-            id={sf1?.id}
-            basePath={basePath}
-            status={sf1?.status}
-            date={formatDate(sf1?.date)}          
-            time={sf1?.time}
-            city={sf1?.city}
-            roundTitle="Polfinale"
-            top={teamRow(sf1?.home, sf1?.homeScore)}
-            bottom={teamRow(sf1?.away, sf1?.awayScore)}
-            classes={classes}
-            isExport={isExport}
-          />
-        </div>
 
-        {/* 2. STOLPEC: FINALA (Na mobitelu order: 3 - pridejo zadnja) */}
-        {!t3 ? (
-          <div className={`${isExport ? classes.col2 : classes.col} ${classes.order3}`}>
-            <Match
-              id={f1?.id}
-              basePath={basePath}
-              status={f1?.status}
-              date={formatDate(f1?.date)} 
-              time={f1?.time}
-              city={f1?.city}
-              roundTitle="Finale za 1. mesto"
-              top={teamRow(f1?.home, f1?.homeScore)}
-              bottom={teamRow(f1?.away, f1?.awayScore)}
-              classes={classes}
-              isExport={isExport}
-            /> 
-          </div>
-        ) : (
-          <div className={`
-            ${isExport ? classes.col2 : classes.col} 
-            ${isExport ? classes.colFinals2 : classes.colFinals} 
-            ${classes.order3}
-          `}>
-            <Match
-              id={f1?.id}
-              basePath={basePath}
-              status={f1?.status}
-              date={formatDate(f1?.date)} 
-              time={f1?.time}
-              city={f1?.city}
-              roundTitle="Finale za 1. mesto"
-              top={teamRow(f1?.home, f1?.homeScore)}
-              bottom={teamRow(f1?.away, f1?.awayScore)}
-              classes={classes}
-              isExport={isExport}
-            /> 
-            <Match
-              id={t3?.id}
-              basePath={basePath}
-              status={t3?.status}
-              date={formatDate(t3?.date)} 
-              time={t3?.time}
-              city={t3?.city}
-              roundTitle="Tekma za 3. mesto"
-              top={teamRow(t3?.home, t3?.homeScore)}
-              bottom={teamRow(t3?.away, t3?.awayScore)}
-              classes={classes}
-              isExport={isExport}
-            />
-          </div>
-        )}
+<div className={isExport ? classes.container2 : classes.container}>
+  
+  {/* 1. STOLPEC: SF1 (Na mobiju prvi, v Exportu levi) */}
+  <div className={`
+    ${isExport ? classes.col2 : classes.col} 
+    ${!isExport ? classes.order1 : ""} 
+  `}>
+    {!isExport && <h3 className={classes.onlyMobileTitle}>Polfinale</h3>}
+    <Match
+      {...sf1}
+      roundTitle={isExport ? "Polfinale" : null}
+      isExport={isExport}
+    />
+  </div>
 
-        {/* 3. STOLPEC: POLFINALE 2 (Na mobitelu order: 1 - da bosta oba polfinala skupaj na vrhu) */}
-        <div className={`
-          ${isExport ? classes.col2 : classes.col} 
-          ${isExport ? classes.colSemi2 : classes.colSemi} 
-          ${classes.order1}
-        `}>
-          <Match
-            id={sf2?.id}
-            basePath={basePath}
-            status={sf2?.status}
-            date={formatDate(sf2?.date)} 
-            time={sf2?.time}
-            city={sf2?.city}
-            roundTitle="Polfinale"
-            top={teamRow(sf2?.home, sf2?.homeScore)}
-            bottom={teamRow(sf2?.away, sf2?.awayScore)}
-            classes={classes}
-            isExport={isExport}
-          />
-        </div>
+  {/* 2. STOLPEC: FINALE (Na mobiju ZADNJI (order 3), v Exportu SREDINSKI) */}
+  <div className={`
+    ${isExport ? classes.col2 : classes.col} 
+    ${!isExport ? classes.order3 : ""} 
+  `}>
+    {!isExport && <h3 className={classes.onlyMobileTitle}>Finalni boji</h3>}
+    {/* Tukaj gresta f1 in t3 eden pod drugega */}
+    <Match {...f1} roundTitle={isExport ? "Finale" : null} isExport={isExport} />
+    {t3 && <Match {...t3} roundTitle={isExport ? "3. mesto" : null} isExport={isExport} />}
+  </div>
 
-      </div>
+  {/* 3. STOLPEC: SF2 (Na mobiju DRUGI (order 1), v Exportu DESNI) */}
+  <div className={`
+    ${isExport ? classes.col2 : classes.col} 
+    ${!isExport ? classes.order1 : ""} 
+  `}>
+    <Match
+      {...sf2}
+      roundTitle={isExport ? "Polfinale" : null}
+      isExport={isExport}
+    />
+  </div>
+
+</div>
     </div>
   );
 }
